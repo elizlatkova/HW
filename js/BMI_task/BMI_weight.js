bd8001ad2262e9847537372df771b2d28ba6c6db
Eli Zlatkova <eli.zlatkova@abv.bg>
2023-12-09 22:39:00 +0200

    BMI updated - console.log(<patient condition>)

--- js/BMI_task/BMI_weight.js
+++ js/BMI_task/BMI_weight.js
@@ -7,6 +7,41 @@ let weightInput = document.querySelector('#weight');
 btn.addEventListener('click', function (e) {
     let height = heightInput.value;
     let weight = weightInput.value;
-    let BMI = Math.round(weight*10 / (height * height)) /10;
+    let BMI = Math.round(weight * 10 / (height * height)) / 10;
     output.innerHTML = BMI;
+
+    if (BMI < 16) {
+        console.log('тежко недохранване');
+    }
+    else if (BMI >= 16 && BMI < 17) {
+        console.log('средно недохранване');
+    }
+    else if (BMI >= 17 && BMI < 18.49) {
+        console.log('леко недохранване');
+    }
+    else if (BMI < 18.5) {
+        console.log('поднормено тегло');
+    }
+    else if (BMI >= 18.5 && BMI < 25) {
+        console.log('средно недохранване');
+    }
+    else if (BMI == 25) {
+        console.log('наднормено тегло');
+    }
+    else if (BMI > 25 && BMI < 30) {
+        console.log('предзатлъстяване');
+    }
+    else if (BMI == 30) {
+        console.log('затлъстяване');
+    }
+    else if (BMI > 30 && BMI < 35) {
+        console.log('затлъстяване I степен');
+    }
+    else if (BMI >= 35 && BMI < 40) {
+        console.log('затлъстяване II степен');
+    }
+    else if (BMI >= 40) {
+        console.log('затлъстяване III степен');
+    }
 });
+
